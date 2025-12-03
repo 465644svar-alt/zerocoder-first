8db19005c3e30e39f0cf1a846f8e7d7b2bbf41c8
465644svar-alt <465644svar@gmail.com>
2025-12-03 16:02:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ print(area_circle(10))
 
 def area_square(a):
     return a ** 2
+
 print(area_square(10))
 
 def area_trapez(a, b,h):
     S_t = 0.5 * (a + b) *h
     return S_t
+
 print(area_trapez(5, 10,5))
